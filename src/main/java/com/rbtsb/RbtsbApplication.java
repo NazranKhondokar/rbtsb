92412287fcd357225ce2b222458ab397fd9c6f22
NazranKhondokar <nazran91@gmail.com>
2023-03-03 21:53:51 +0600

    init commit

---
+++ src/main/java/com/rbtsb/RbtsbApplication.java
@@ -0,0 +1,13 @@
+package com.rbtsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbtsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbtsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbtsb/RbtsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbtsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbtsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
