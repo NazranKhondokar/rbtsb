61e9a5f1237ac23dd2e1c497b9c9a403acf199ed
nazran <nazran@eatlbd.com>
2023-03-05 14:12:26 +0600

    data fetched from citylinkexpress

--- src/main/java/com/rbtsb/services/CourierService.java
+++ src/main/java/com/rbtsb/services/CourierService.java
@@ -1,26 +1,9 @@
 package com.rbtsb.services;
 
-import com.rbtsb.clients.CourierClient;
 import com.rbtsb.dto.courier.CourierPriceDataDto;
-import com.rbtsb.dto.courier.CourierResponseDto;
-import com.rbtsb.exceptions.ResourceNotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class CourierService {
+public interface CourierService {
 
-    private final CourierClient courierClient;
-
-    public CourierPriceDataDto getCourierPrice() {
-        CourierResponseDto courierResponseDto = courierClient.getCourierPrice(
-                "MY", "Kuala Lumpur", 50250, "BD",
-                "Bangladesh", 50000, 10, 10, 10,
-                1, 10, 0);
-        if (courierResponseDto.getReq().getStatus() == 200)
-            return courierResponseDto.getReq().getData();
-        else throw new ResourceNotFoundException(String.valueOf(courierResponseDto.getReq().getStatus()));
-    }
+    CourierPriceDataDto getCourierPrice();
 }
 
---
+++ src/main/java/com/rbtsb/services/implementations/CourierServiceImpl.java
@@ -0,0 +1,27 @@
+package com.rbtsb.services.implementations;
+
+import com.rbtsb.clients.CourierClient;
+import com.rbtsb.dto.courier.CourierPriceDataDto;
+import com.rbtsb.dto.courier.CourierResponseDto;
+import com.rbtsb.exceptions.ResourceNotFoundException;
+import com.rbtsb.services.CourierService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourierServiceImpl implements CourierService {
+
+    private final CourierClient courierClient;
+
+    @Override
+    public CourierPriceDataDto getCourierPrice() {
+        CourierResponseDto courierResponseDto = courierClient.getCourierPrice(
+                "MY", "Kuala Lumpur", 50250, "BD",
+                "Bangladesh", 50000, 10, 10, 10,
+                1, 10, 0);
+        if (courierResponseDto.getReq().getStatus() == 200)
+            return courierResponseDto.getReq().getData();
+        else throw new ResourceNotFoundException(String.valueOf(courierResponseDto.getReq().getStatus()));
+    }
+}
