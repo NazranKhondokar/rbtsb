2ed6f6d30e7530d8bb8de0cca01e8948ec088d7b
NazranKhondokar <nazran91@gmail.com>
2023-03-10 03:08:09 +0600

    refactor

--- src/main/java/com/rbtsb/repositories/CourierPriceRepository.java
+++ src/main/java/com/rbtsb/repositories/CourierPriceRepository.java
@@ -2,8 +2,28 @@ package com.rbtsb.repositories;
 
 import com.rbtsb.entities.CourierPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourierPriceRepository extends JpaRepository<CourierPrice, Long> {
+
+    @Query(value = """
+            SELECT cp.* FROM COURIER_PRICE AS cp WHERE
+            (:originPostcode IS NULL OR :originPostcode = cp.ORIGIN_POSTCODE) AND
+            (:destinationCountry IS NULL OR LOWER(:destinationCountry) = LOWER(cp.DESTINATION_COUNTRY)) AND
+            (:destinationPostcode IS NULL OR :destinationPostcode = cp.DESTINATION_POSTCODE) AND
+            (:width IS NULL OR :width = cp.WIDTH) AND
+            (:height IS NULL OR :height = cp.HEIGHT) AND
+            (:parcelWeight IS NULL OR :parcelWeight = cp.PARCEL_WEIGHT)
+            """,
+            nativeQuery = true)
+    List<CourierPrice> search(Integer originPostcode,
+                              String destinationCountry,
+                              Integer destinationPostcode,
+                              Double width,
+                              Double height,
+                              Double parcelWeight);
 }
--- src/main/java/com/rbtsb/services/implementations/CourierPriceCheckServiceImpl.java
+++ src/main/java/com/rbtsb/services/implementations/CourierPriceCheckServiceImpl.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 @Service
@@ -29,18 +30,54 @@ public class CourierPriceCheckServiceImpl implements CourierPriceCheckService {
 
     @Override
     public List<CourierRateResponse> checkCourierPrice(CourierPriceCheckDto courierPriceCheckDto) {
-
         List<CourierRateResponse> courierRateResponses = new ArrayList<>();
-        CityLinkPriceDataDto cityLinkPriceDataDto = cityLinkService.getCourierPrice(courierPriceCheckDto);
+
+        List<CourierPrice> courierPrices = getCourierPrices(courierPriceCheckDto);
+        logger.info("courierPrices {}", courierPrices);
 
         CourierPrice courierPrice = courierPriceCheckDto.to();
+
+        if (courierPrices.isEmpty()) {
+            logger.info("courierPrices is empty");
+            getCityLinkRate(courierPriceCheckDto, courierRateResponses, courierPrice);
+            getJTRate(courierPriceCheckDto, courierRateResponses, courierPrice);
+            courierPriceRepository.save(courierPrice);
+        } else {
+            logger.info("courierPrices is not empty");
+            if (isNull(courierPrices.get(0).getCityLinkRate()))
+                getCityLinkRate(courierPriceCheckDto, courierRateResponses, courierPrice);
+            else courierRateResponses.add(CourierRateResponse.to("citylink", courierPrices.get(0).getCityLinkRate()));
+
+            if (isNull(courierPrices.get(0).getJtRate()))
+                getJTRate(courierPriceCheckDto, courierRateResponses, courierPrice);
+            else courierRateResponses.add(CourierRateResponse.to("jt", courierPrices.get(0).getJtRate()));
+        }
+        return courierRateResponses;
+    }
+
+    private List<CourierPrice> getCourierPrices(CourierPriceCheckDto courierPriceCheckDto) {
+        // TODO I wanna check duplicate API call via redis but time is up ...
+        // it's a dummy check
+        return courierPriceRepository.search(
+                courierPriceCheckDto.getOriginPostcode(),
+                courierPriceCheckDto.getDestinationCountry(),
+                courierPriceCheckDto.getDestinationPostcode(),
+                courierPriceCheckDto.getWidth(),
+                courierPriceCheckDto.getHeight(),
+                courierPriceCheckDto.getParcelWeight());
+    }
+
+    private void getCityLinkRate(CourierPriceCheckDto courierPriceCheckDto, List<CourierRateResponse> courierRateResponses, CourierPrice courierPrice) {
+        CityLinkPriceDataDto cityLinkPriceDataDto = cityLinkService.getCourierPrice(courierPriceCheckDto);
 
         if (nonNull(cityLinkPriceDataDto)) {
             logger.info("CityLink express rate is " + cityLinkPriceDataDto.getRate());
             courierPrice.setCityLinkRate(cityLinkPriceDataDto.getRate());
             courierRateResponses.add(CourierRateResponse.to("citylink", cityLinkPriceDataDto.getRate()));
         } else logger.warn("CityLink express data is null");
+    }
 
+    private void getJTRate(CourierPriceCheckDto courierPriceCheckDto, List<CourierRateResponse> courierRateResponses, CourierPrice courierPrice) {
         try {
             JTPriceDataDto jtPriceDataDto = jtService.getCourierPrice(courierPriceCheckDto);
             if (nonNull(jtPriceDataDto.getTotalRate())) {
@@ -50,7 +87,5 @@ public class CourierPriceCheckServiceImpl implements CourierPriceCheckService {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-        courierPriceRepository.save(courierPrice);
-        return courierRateResponses;
     }
 }
